3b8bf0bfce0515ce294c4ffda5fad7f112f22ed8
Brett Peacock <brett@soap.com.au>
2018-06-07 12:16:50 +1000

    php update and source directory

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+
+if (isset($_GET['info']) && $_GET['info'] == "robbie") {
+        phpinfo();
+} else {
+    '
+    <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Centos deployment</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: orange;
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {
+      font-size: 200%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+  </style>
+</head>
+<body>
+  <div align="center">
+    <h1>CENTOS!</h1>
+    <h2>1 This application was deployed using AWS CodeDeploy.</h2>
+    <p>For next steps, read the <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Documentation</a>.</p>
+  </div>
+</body>
+</html>
+';
+}
+
+?>
